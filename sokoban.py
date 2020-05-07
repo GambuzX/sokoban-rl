0d57f448fb1d07736f39afc8947ac10c57fc50cd
GambuzX <mg_mesquita@hotmail.com>
2020-05-08 00:17:49 +0100

    Understood what the step return values were and wrote in comments

--- sokoban.py
+++ sokoban.py
@@ -4,6 +4,15 @@ import gym_sokoban
 env = gym.make('Sokoban-v0')
 env.reset()
 
+'''
+* action is a number from 0 to 8 specifying the action, as in the gym-sokoban repo
+* step(action) return:
+    - observation: board's pixels (width x height). each element represents the rgb color of the pixel in human mode
+    - reward: double value representing the reward, as explained in the gym-sokoban repo
+    - done: boolean, has episode terminated
+    - info: {'action.name': 'push down (example)', 'action.moved_player': BOOLEAN, 'action.moved_box': BOOLEAN}
+'''
+
 for _ in range(1000):
     env.render()
     action = env.action_space.sample()
