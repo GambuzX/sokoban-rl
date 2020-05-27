baea9f8771227e2064e538bd1df784cf58637a69
Pedro Esteves <up201705160@fe.up.pt>
2020-05-27 17:34:48 +0100

    Added qlearning test. Changed while to for with list

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import gym
 import gym_sokoban
 from algorithms.sarsa import run_sarsa
 from algorithms.montecarlo import run_montecarlo
+from algorithms.qlearning import run_qlearning
 from sokoban_utils.policy import run_policy
 from sokoban_utils.utils import *
 from sokoban_utils.global_configs import GlobalConfigs
+import numpy as np
 
 env = gym.make('Boxoban-Train-v1')
  
@@ -13,26 +15,37 @@ config.total_episodes = 5
 
 create_dir(GlobalConfigs.logs_dir)        
 
-# Test sarsa alpha (learning rate)
+# Test sarsa and qlearning alpha (learning rate)
 sarsa_alpha = open("logs/sarsa_alpha.txt", "w")
+qlearning_alpha = open("logs/qlearning_alpha.txt", "w")
 
-sarsa_alpha_config = copy_config(config)
-learning_rate = 0.1
-while learning_rate <= 1.0:
-    sarsa_alpha_config.alpha = learning_rate
-    _, logfile = run_sarsa(env, log=True, initial_config=sarsa_alpha_config)
+alpha_config = copy_config(config)
+
+# Defining list of values for the learning rate (alpha)
+learning_rate_list = np.arange(0.1, 1.0, 0.1).tolist()
+
+for learning_rate in learning_rate_list:
+    alpha_config.alpha = learning_rate
+    _, logfile = run_sarsa(env, log=True, initial_config=alpha_config)
     sarsa_alpha.write(logfile + "\n")
-    learning_rate += 0.1
+    
+    _, logfile = run_qlearning(env, log=True, initial_config=alpha_config)
+    qlearning_alpha.write(logfile + "\n")
     
 sarsa_alpha.close()
+qlearning_alpha.close()
 
-#Test montecarlo and sarsa gamma (discounting rate) 
+#Test montecarlo, sarsa and qlearning gamma (discounting rate) 
 sarsa_gamma = open("logs/sarsa_gamma.txt", "w")
 montecarlo_gamma = open("logs/montecarlo_gamma.txt", "w")
+qlearning_gamma = open("logs/qlearning_gamma.txt", "w")
 
 gamma_config = copy_config(config)
-discounting_rate = 0.1
-while discounting_rate <= 1.0:
+
+# Defining list of values for the discounting rate (gamma)
+discounting_rate_list = np.arange(0.1, 1.0, 0.1).tolist()
+
+for discounting_rate in discounting_rate_list:
     gamma_config.gamma = discounting_rate
     _, logfile = run_sarsa(env, log=True, initial_config=gamma_config)
     sarsa_gamma.write(logfile + "\n")
@@ -40,19 +53,25 @@ while discounting_rate <= 1.0:
     _, logfile = run_montecarlo(env, log=True, initial_config=gamma_config)
     montecarlo_gamma.write(logfile + "\n")
     
-    discounting_rate += 0.1
+    _, logfile = run_qlearning(env, log=True, initial_config=gamma_config)
+    qlearning_gamma.write(logfile + "\n")
 
 sarsa_gamma.close()
 montecarlo_gamma.close()
+qlearning_gamma.close()
 
 
-#Test montecarlo and sarsa max epsilon (exploration probability at start) 
+#Test montecarlo, sarsa and qlearning max epsilon (exploration probability at start) 
 sarsa_max_epsilon = open("logs/sarsa_max_epsilon.txt", "w")
 montecarlo_max_epsilon = open("logs/montecarlo_max_epsilon.txt", "w")
+qlearning_max_epsilon = open("logs/qlearning_max_epsilon.txt", "w")
 
 max_epsilon_config = copy_config(config)
-max_epsilon = 0.1
-while max_epsilon <= 1.0:
+
+# Defining list of values for the max epsilon
+max_epsilon_list = np.arange(0.1, 1.0, 0.1).tolist()
+
+for max_epsilon in max_epsilon_list:
     max_epsilon_config.max_epsilon = max_epsilon
     _, logfile = run_sarsa(env, log=True, initial_config=max_epsilon_config)
     sarsa_max_epsilon.write(logfile + "\n")
@@ -60,9 +79,11 @@ while max_epsilon <= 1.0:
     _, logfile = run_montecarlo(env, log=True, initial_config=max_epsilon_config)
     montecarlo_max_epsilon.write(logfile + "\n")
     
-    max_epsilon += 0.1
+    _, logfile = run_qlearning(env, log=True, initial_config=max_epsilon_config)
+    qlearning_max_epsilon.write(logfile + "\n")
 
 sarsa_max_epsilon.close()
 montecarlo_max_epsilon.close()
+qlearning_max_epsilon.close()
 
 env.close()
